74a1024f5aa9a51e3fb9cb21dff45b6e35709361
Jenya Trofimov <deadrime@yandex.ru>
2019-07-07 13:25:05 +0300

    Header styles

--- src/components/AccountMenu/index.js
+++ src/components/AccountMenu/index.js
@@ -2,10 +2,10 @@ import { createElement as $ } from 'react'
 import ArrowDownIcon from '@/assets/svg/icon-header-arr-down.svg'
 import styles from './style.styl'
 
-const AccountMenu = () => 
+const AccountMenu = ({ amount = 4700 }) => 
   $('div', { className: styles.accountMenu},
     $('span', { className: styles.amountText }, 'Ваш счет'),
-    $('span', { className: styles.amountValue }, `${4700} ₽`),
+    $('span', { className: styles.amountValue }, `${amount.toLocaleString()} ₽`),
     $(ArrowDownIcon, { className: styles.icon }))
 
 export default AccountMenu
