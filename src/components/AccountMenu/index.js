cddcf298472586332e849977efdfe4f2a16af3af
Jenya Trofimov <deadrime@yandex.ru>
2019-07-08 15:22:50 +0300

    Amount from store

--- src/components/AccountMenu/index.js
+++ src/components/AccountMenu/index.js
@@ -2,11 +2,12 @@ import { createElement as $ } from 'react'
 import ArrowDownIcon from '@/assets/svg/icon-header-arr-down.svg'
 import styles from './style.styl'
 import { formatMoney } from '@/helpers'
+import { withCartStore } from '@/store/cartStore'
 
-const AccountMenu = ({ amount = 4700 }) => 
+const AccountMenu = ({ store: { accountAmount } }) =>
   $('div', { className: styles.accountMenu, role: 'button', tabIndex: 0 },
     $('span', { className: styles.amountText }, 'Ваш счет'),
-    $('span', { className: styles.amountValue }, formatMoney(amount)),
+    $('span', { className: styles.amountValue }, formatMoney(accountAmount)),
     $(ArrowDownIcon, { className: styles.icon }))
 
-export default AccountMenu
+export default withCartStore(AccountMenu)
